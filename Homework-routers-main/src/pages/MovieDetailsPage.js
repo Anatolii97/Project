16b74e945ebf3e98c3bba7f5981b6b4990a95fcc
Anatolii97 <87384010+Anatolii97@users.noreply.github.com>
2021-11-14 11:40:12 +0200

    Delete MovieDetailsPage.js

--- Homework-routers-main/src/pages/MovieDetailsPage.js
+++
@@ -1,5 +0,0 @@
-import { MovieDetailsContainer } from "../components/MovieDetails/MovieDetails"
-
-export const MovieDetails = ({match, history}) => {
-    return <MovieDetailsContainer match={match} history={history} />
-}
